722a7116b9e9f7609591640cad864700dcfa772c
ACampero <andresmiamifan@hotmail.com>
2020-03-26 12:21:24 -0400

    mistake

--- hsp/trainer.py
+++ hsp/trainer.py
@@ -161,6 +161,7 @@ class Trainer(object):
             action_out = [torch.cat(a, dim=0) for a in action_out]
         else:
             # forward again in batch for speed-up
+            print('hola_batch', len(batch), batch[0].state.shape, type(batch[0].state))
             states = Variable(torch.cat(batch.state, dim=0), requires_grad=False)
             if self.args.sp:
                 action_out, values, goal_vectors, enc_vectors = self.policy_net(states)
